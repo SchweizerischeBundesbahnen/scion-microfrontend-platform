20a3266823ce1a3f82063e87e18235988a6ed478
Marcarrian <marc.muster@bluewin.ch>
2020-07-17 17:42:53 +0200

    chore: update project workspace to Angular 10
    The project workspace of the SCION Microfrontend Platform depends on the Angular framework because:
    - the internal test app is an Angular application
    - the project is packaged using Angular ng-packagr
    
    At runtime, however, the platform has no dependence on the Angular framework.
    
    closes #19
    
    BREAKING CHANGE: esm5 and fesm5 format is no longer distributed in `@scion/microfrontend-platform`’s NPM package
    
    We no longer include the distributions for `esm5` and `fesm5` in the `@scion/microfrontend-platform`’s NPM package. Only the formats for `esm2015`, `fesm2015`, and UMD are distributed. Consequently, the module field in package.json now points to the `fesm2015` distribution.
    
    To migrate:
    - If requiring `esm5` or `fesm5`, you will need to downlevel to ES5 yourself. If using Angular, the Angular CLI will automatically downlevel the code to ES5 if differential loading is enabled in the Angular project, so no action is required from Angular CLI users.

--- projects/scion/microfrontend-platform.e2e/protractor.conf.js
+++ projects/scion/microfrontend-platform.e2e/protractor.conf.js
@@ -11,7 +11,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const {SpecReporter} = require('jasmine-spec-reporter');
+const {SpecReporter, StacktraceOption} = require('jasmine-spec-reporter');
 
 const puppeteer = require('puppeteer');
 const chromeArgs = ['--window-size=2048,1536'];
@@ -76,6 +76,10 @@ exports.config = {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.json'),
     });
-    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: StacktraceOption.PRETTY
+      }
+    }));
   },
 };
