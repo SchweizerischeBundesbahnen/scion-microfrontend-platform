d0f5f13b26c6c1d31fdb3452cd87050b8a2daf2d
danielwiehl <daniel.wiehl@yahoo.com>
2022-05-18 16:55:29 +0200

    release: v1.0.0-rc.5

--- projects/scion/microfrontend-platform/src/lib/microfrontend-platform.ts
+++ projects/scion/microfrontend-platform/src/lib/microfrontend-platform.ts
@@ -58,7 +58,7 @@ import {ɵClientRegistry} from './host/client-registry/ɵclient.registry';
 /**
  * Current version of the SCION Microfrontend Platform.
  */
-const version = '1.0.0-rc.4';
+const version = '1.0.0-rc.5';
 
 /**
  * **SCION Microfrontend Platform is a TypeScript-based open-source library that helps to implement a microfrontend architecture.**
