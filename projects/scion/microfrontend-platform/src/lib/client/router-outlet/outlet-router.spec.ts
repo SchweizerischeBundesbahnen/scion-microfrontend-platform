4c88ccd3adcba31105117c44d44c465a765990e4
danielwiehl <daniel.wiehl@yahoo.com>
2023-11-10 11:23:18 +0100

    fix(platform): support navigating to blob URL
    ```ts
    // Create blob URL
    const content = '<html><body>Blob Content</body></html>';
    const blobUrl = URL.createObjectURL(new Blob([content], {type: 'text/html'}));
    
    // Navigate to the blob URL
    Beans.get(OutletRouter).navigate(blobUrl);
    ```

--- projects/scion/microfrontend-platform/src/lib/client/router-outlet/outlet-router.spec.ts
+++ projects/scion/microfrontend-platform/src/lib/client/router-outlet/outlet-router.spec.ts
@@ -11,11 +11,42 @@ import {MicrofrontendPlatform} from '../../microfrontend-platform';
 import {MicrofrontendPlatformHost} from '../../host/microfrontend-platform-host';
 import {Beans} from '@scion/toolkit/bean-manager';
 import {OutletRouter} from './outlet-router';
+import {SciRouterOutletElement} from './router-outlet.element';
+import {fromEvent, lastValueFrom} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 describe('OutletRouter', () => {
 
   beforeEach(async () => await MicrofrontendPlatform.destroy());
   afterEach(async () => await MicrofrontendPlatform.destroy());
+
+  it('should support navigating to blob URL', async () => {
+    await MicrofrontendPlatformHost.start({applications: []});
+
+    // Add <sci-router-outlet> element to the DOM.
+    const sciRouterOutlet = document.body.appendChild(document.createElement('sci-router-outlet') as SciRouterOutletElement);
+    sciRouterOutlet.name = 'outlet';
+    sciRouterOutlet.style.height = '150px';
+    sciRouterOutlet.style.width = '300px';
+    sciRouterOutlet.style.border = '1px solid black';
+
+    // Create blob URL.
+    const content = '<html><body><main>Blob Content</main></body></html>';
+    const blobUrl = URL.createObjectURL(new Blob([content], {type: 'text/html'}));
+
+    // Navigate to blob URL.
+    Beans.get(OutletRouter).navigate(blobUrl, {outlet: 'outlet'}).then();
+
+    // Wait until loaded the blob page into the iframe.
+    // Since the iframe is initialized with the "about:blank" page, we expect the blob page load event to fire as the second load event.
+    await lastValueFrom(fromEvent(sciRouterOutlet.iframe, 'load').pipe(take(2)));
+
+    // Expect the blob page to display.
+    expect((sciRouterOutlet.iframe.contentWindow!.document.querySelector('main'))!.innerText).toEqual('Blob Content');
+
+    // Cleanup resources.
+    URL.revokeObjectURL(blobUrl);
+  });
 
   describe('intent-based-routing', () => {
     it('should reject navigation if passing "relativeTo" navigation option', async () => {
