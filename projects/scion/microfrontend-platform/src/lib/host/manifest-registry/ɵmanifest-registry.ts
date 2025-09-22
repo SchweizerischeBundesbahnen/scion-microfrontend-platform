8bed7f54bbbff5df63f2022b31b1189f375bb731
danielwiehl <daniel.wiehl@yahoo.com>
2025-09-22 15:16:22 +0200

    feat(platform): support rejecting capability in a capability interceptor
    Returning `null` in a `CapabilityInterceptor` prevents the capability from being registered, e.g, based on user permissions.
    
    Example:
    ```ts
    class UserAuthorizedCapabilityInterceptor implements CapabilityInterceptor {
    
      public async intercept(capability: Capability): Promise<Capability | null> {
        const requiredRole = capability.properties?.['role'];
    
        // `hasRole` is illustrative and not part of the Microfrontend Platform API
        return !requiredRole || hasRole(requiredRole) ? capability : null;
      }
    }
    
    // Register the interceptor.
    Beans.register(CapabilityInterceptor, {useClass: UserAuthorizedCapabilityInterceptor, multi: true});
    ```
    
    BREAKING CHANGE: `ManifestService.registerCapability` now returns `null` if an interceptor prevented registration
    
    closes #311

--- projects/scion/microfrontend-platform/src/lib/host/manifest-registry/ɵmanifest-registry.ts
+++ projects/scion/microfrontend-platform/src/lib/host/manifest-registry/ɵmanifest-registry.ts
@@ -118,7 +118,7 @@ export class ɵManifestRegistry implements ManifestRegistry, PreDestroy {
     }).length > 0;
   }
 
-  public async registerCapability(capability: Capability, appSymbolicName: string): Promise<string> {
+  public async registerCapability(capability: Capability, appSymbolicName: string): Promise<string | null> {
     if (!capability) {
       throw Error('[CapabilityRegisterError] Capability must not be null or undefined.');
     }
@@ -145,8 +145,11 @@ export class ɵManifestRegistry implements ManifestRegistry, PreDestroy {
     });
 
     // Register the capability.
-    this._capabilityStore.add(capabilityToRegister);
-    return capabilityToRegister.metadata!.id;
+    if (capabilityToRegister) {
+      this._capabilityStore.add(capabilityToRegister);
+      return capabilityToRegister.metadata!.id;
+    }
+    return null;
   }
 
   public unregisterCapabilities(appSymbolicName: string, filter: ManifestObjectFilter): void {
@@ -183,7 +186,7 @@ export class ɵManifestRegistry implements ManifestRegistry, PreDestroy {
   }
 
   private installCapabilityRegisterRequestHandler(): void {
-    this._subscriptions.add(Beans.get(MessageClient).onMessage<Capability, string>(PlatformTopics.RegisterCapability, (request: TopicMessage<Capability>) => {
+    this._subscriptions.add(Beans.get(MessageClient).onMessage<Capability, string | null>(PlatformTopics.RegisterCapability, (request: TopicMessage<Capability>) => {
       const capability = request.body!;
       const appSymbolicName = request.headers.get(MessageHeaders.AppSymbolicName);
       return this.registerCapability(capability, appSymbolicName);
@@ -335,12 +338,11 @@ function assertCapabilityParamDefinitions(params: ParamDefinition[] | undefined)
 /**
  * Intercepts capability before its registration.
  */
-async function interceptCapability(capability: Capability): Promise<Capability> {
+async function interceptCapability(capability: Capability): Promise<Capability | null> {
   const interceptors = Beans.all(CapabilityInterceptor);
-
   const appSymbolicName = capability.metadata!.appSymbolicName;
   const manifest = new class implements CapabilityInterceptor.Manifest {
-    public addCapability(capability: Capability): Promise<string> {
+    public addCapability(capability: Capability): Promise<string | null> {
       return Beans.get(ManifestRegistry).registerCapability(capability, appSymbolicName);
     }
 
@@ -349,8 +351,12 @@ async function interceptCapability(capability: Capability): Promise<Capability> 
     }
   }();
 
+  let interceptedCapability: Capability | null = capability;
   for (const interceptor of interceptors) {
-    capability = await interceptor.intercept(capability, manifest);
+    interceptedCapability = await interceptor.intercept(interceptedCapability!, manifest);
+    if (interceptedCapability === null) {
+      break; // rejected by the interceptor
+    }
   }
-  return capability;
+  return interceptedCapability;
 }
