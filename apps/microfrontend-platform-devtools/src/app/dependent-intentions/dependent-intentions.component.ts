4daac8e0b6a4b643d044b7a5a117dd0b05cad1a9
Konstantin Genov <konstantin.genov2@sbb.ch>
2025-09-30 17:28:50 +0200

    refactor(testapp/devtools): migrate components to signal inputs, outputs and queries
    closes #305, #306

--- apps/microfrontend-platform-devtools/src/app/dependent-intentions/dependent-intentions.component.ts
+++ apps/microfrontend-platform-devtools/src/app/dependent-intentions/dependent-intentions.component.ts
@@ -7,16 +7,15 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {ChangeDetectionStrategy, Component, inject, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, inject, input, signal, untracked} from '@angular/core';
 import {Intention} from '@scion/microfrontend-platform';
 import {Router} from '@angular/router';
-import {Observable, ReplaySubject} from 'rxjs';
-import {expand, map, switchMap, take} from 'rxjs/operators';
+import {expand, map, take} from 'rxjs/operators';
 import {filterManifestObjects} from '../common/manifest-object-filter.utils';
 import {DevToolsManifestService} from '../dev-tools-manifest.service';
 import {Maps} from '@scion/toolkit/util';
 import {NonNullableFormBuilder, ReactiveFormsModule} from '@angular/forms';
-import {AsyncPipe, KeyValuePipe} from '@angular/common';
+import {KeyValuePipe} from '@angular/common';
 import {AppNamePipe} from '../common/app-name.pipe';
 import {SciQualifierChipListComponent} from '@scion/components.internal/qualifier-chip-list';
 import {SciAccordionComponent, SciAccordionItemDirective} from '@scion/components.internal/accordion';
@@ -29,7 +28,6 @@ import {SciMaterialIconDirective} from '@scion/components.internal/material-icon
   styleUrls: ['./dependent-intentions.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    AsyncPipe,
     KeyValuePipe,
     ReactiveFormsModule,
     SciFilterFieldComponent,
@@ -40,36 +38,42 @@ import {SciMaterialIconDirective} from '@scion/components.internal/material-icon
     SciMaterialIconDirective,
   ],
 })
-export class DependentIntentionsComponent implements OnInit, OnChanges {
+export class DependentIntentionsComponent {
+
+  public readonly appSymbolicName = input.required<string>();
 
   private readonly _router = inject(Router);
   private readonly _formBuilder = inject(NonNullableFormBuilder);
   private readonly _manifestService = inject(DevToolsManifestService);
-  private readonly _appChange$ = new ReplaySubject<void>(1);
 
-  @Input({required: true})
-  public appSymbolicName!: string;
+  protected readonly intentionsByApp = signal(new Map<string, Intention[]>());
+  protected readonly filterFormControl = this._formBuilder.control('');
 
-  public intentionsByApp$: Observable<Map<string, Intention[]>> | undefined;
-  public filterFormControl = this._formBuilder.control('');
-
-  public ngOnInit(): void {
-    this.intentionsByApp$ = this._appChange$
-      .pipe(
-        switchMap(() => this._manifestService.observeDependentIntentions$(this.appSymbolicName)),
-        expand(intentions => this.filterFormControl.valueChanges.pipe(take(1), map(() => intentions))),
-        map(intentions => filterManifestObjects(intentions, this.filterFormControl.value)),
-        map(intentions => intentions.reduce((acc, intention) => Maps.addListValue(acc, intention.metadata!.appSymbolicName, intention), new Map<string, Intention[]>())),
-      );
+  constructor() {
+    this.computeIntentionsByApp();
   }
 
-  public ngOnChanges(changes: SimpleChanges): void {
-    this.filterFormControl.reset('');
-    this._appChange$.next();
-  }
-
-  public onOpenAppClick(event: MouseEvent, appSymbolicName: string): void {
+  protected onOpenAppClick(event: MouseEvent, appSymbolicName: string): void {
     event.stopPropagation();
     void this._router.navigate(['/apps', {outlets: {details: [appSymbolicName]}}]);
+  }
+
+  private computeIntentionsByApp(): void {
+    effect(onCleanup => {
+      const appSymbolicName = this.appSymbolicName();
+
+      untracked(() => {
+        this.filterFormControl.reset('');
+
+        const subscription = this._manifestService.observeDependentIntentions$(appSymbolicName)
+          .pipe(
+            expand(intentions => this.filterFormControl.valueChanges.pipe(take(1), map(() => intentions))),
+            map(intentions => filterManifestObjects(intentions, this.filterFormControl.value)),
+            map(intentions => intentions.reduce((acc, intention) => Maps.addListValue(acc, intention.metadata!.appSymbolicName, intention), new Map<string, Intention[]>())),
+          )
+          .subscribe(intentionsByApp => this.intentionsByApp.set(intentionsByApp));
+        onCleanup(() => subscription.unsubscribe());
+      });
+    });
   }
 }
