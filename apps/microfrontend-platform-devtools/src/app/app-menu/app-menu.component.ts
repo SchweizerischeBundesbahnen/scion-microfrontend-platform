79f560db088a9cdb05c818c89f64af523b792810
Konstantin Genov <konstantin.genov2@sbb.ch>
2025-09-30 17:28:50 +0200

    refactor(testapp/devtools): migrate components to signal inputs, outputs and queries
    closes #305, #306

--- apps/microfrontend-platform-devtools/src/app/app-menu/app-menu.component.ts
+++ apps/microfrontend-platform-devtools/src/app/app-menu/app-menu.component.ts
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {ChangeDetectionStrategy, Component, EventEmitter, HostListener, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostListener, output} from '@angular/core';
 import {animate, AnimationMetadata, style, transition, trigger} from '@angular/animations';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {SciViewportComponent} from '@scion/components/viewport';
@@ -30,25 +30,24 @@ import {SciMaterialIconDirective} from '@scion/components.internal/material-icon
 })
 export class AppMenuComponent {
 
-  @Output()
-  public close = new EventEmitter<void>(); // eslint-disable-line @angular-eslint/no-output-native
+  public readonly close = output<void>(); // eslint-disable-line @angular-eslint/no-output-native
 
   @HostListener('document:keydown.escape')
-  public onEscape(): void {
+  protected onEscape(): void {
     this.close.emit();
   }
 
   @HostListener('mousedown', ['$event'])
-  public onHostCloseEvent(event: Event): void {
+  protected onHostCloseEvent(event: Event): void {
     event.stopPropagation(); // Prevent closing this overlay if emitted from a child of this overlay.
   }
 
   @HostListener('document:mousedown')
-  public onDocumentCloseEvent(): void {
+  protected onDocumentCloseEvent(): void {
     this.close.emit();
   }
 
-  public onMenuItemClick(event: MouseEvent): void {
+  protected onMenuItemClick(event: MouseEvent): void {
     event.preventDefault(); // Prevent href navigation imposed by accessibility rules
     this.close.emit();
   }
--- apps/microfrontend-platform-testing-app/src/app/browser-outlet/browser-outlet.component.ts
+++ apps/microfrontend-platform-testing-app/src/app/browser-outlet/browser-outlet.component.ts
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, DestroyRef, ElementRef, inject, Injector, Input, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, effect, ElementRef, inject, Injector, input, untracked, viewChild} from '@angular/core';
 import {MessageClient, OutletRouter, SciRouterOutletElement} from '@scion/microfrontend-platform';
 import {NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Overlay} from '@angular/cdk/overlay';
@@ -17,7 +17,6 @@ import {ActivatedRoute} from '@angular/router';
 import {Beans} from '@scion/toolkit/bean-manager';
 import {environment} from '../../environments/environment';
 import {TestingAppTopics} from '../testing-app.topics';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {SciMaterialIconDirective} from '@scion/components.internal/material-icon';
 
 /**
@@ -34,81 +33,70 @@ import {SciMaterialIconDirective} from '@scion/components.internal/material-icon
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA], // required because <sci-router-outlet> is a custom element
 })
-export class BrowserOutletComponent implements OnInit {
+export class BrowserOutletComponent {
+
+  public readonly outletName = input.required<string>();
 
   private readonly _formBuilder = inject(NonNullableFormBuilder);
   private readonly _activatedRoute = inject(ActivatedRoute);
   private readonly _overlay = inject(Overlay);
   private readonly _injector = inject(Injector);
-  private readonly _destroyRef = inject(DestroyRef);
+  private readonly _settingsButton = viewChild.required<ElementRef<HTMLButtonElement>>('settings_button');
+  private readonly _contextDefineButton = viewChild.required<ElementRef<HTMLButtonElement>>('context_define_button');
+  private readonly _routerOutlet = viewChild.required<ElementRef<SciRouterOutletElement>>('router_outlet');
 
-  public form = this._formBuilder.group({
+  protected readonly appEntryPoints: AppEndpoint[];
+  protected readonly form = this._formBuilder.group({
     url: this._formBuilder.control(null, Validators.required),
   });
-  public appEntryPoints: AppEndpoint[];
-
-  @Input({required: true})
-  public outletName!: string;
-
-  @ViewChild('settings_button', {static: true})
-  public settingsButton!: ElementRef<HTMLButtonElement>;
-
-  @ViewChild('context_define_button', {static: true})
-  public contextDefineButton!: ElementRef<HTMLButtonElement>;
-
-  @ViewChild('router_outlet', {static: true})
-  public routerOutlet!: ElementRef<SciRouterOutletElement>;
 
   constructor() {
     this.appEntryPoints = this.readAppEntryPoints();
+    this.installOutletContextUpdateHandler();
   }
 
-  public ngOnInit(): void {
-    this.installOutletContextUpdateHandler(this.outletName);
-  }
-
-  public onUrlClearClick(): void {
+  protected onUrlClearClick(): void {
     this.form.reset();
     this.navigate();
   }
 
-  public onNavigateClick(): boolean {
+  protected onNavigateClick(): boolean {
     this.navigate();
     return false;
   }
 
-  private navigate(): void {
-    void Beans.get(OutletRouter).navigate(this.form.controls.url.value, {outlet: this.outletName});
-  }
-
-  public onSettingsClick(): void {
+  protected onSettingsClick(): void {
     RouterOutletSettingsComponent.openAsOverlay({
-      anchor: this.settingsButton.nativeElement,
+      anchor: this._settingsButton().nativeElement,
       overlay: this._overlay,
-      routerOutlet: this.routerOutlet.nativeElement,
+      routerOutlet: this._routerOutlet().nativeElement,
       injector: this._injector,
     });
   }
 
-  public onContextDefineClick(): void {
+  protected onContextDefineClick(): void {
     RouterOutletContextComponent.openAsOverlay({
-      anchor: this.contextDefineButton.nativeElement,
+      anchor: this._contextDefineButton().nativeElement,
       overlay: this._overlay,
-      routerOutlet: this.routerOutlet.nativeElement,
+      routerOutlet: this._routerOutlet().nativeElement,
       injector: this._injector,
     });
   }
 
-  public onActivate(event: Event): void {
-    console.debug(`[BrowserOutletComponent::sci-router-outlet:onactivate] [outlet=${this.outletName}, url=${(event as CustomEvent).detail}]`);
+  protected onActivate(event: Event): void {
+    console.debug(`[BrowserOutletComponent::sci-router-outlet:onactivate] [outlet=${this.outletName()}, url=${(event as CustomEvent).detail}]`);
   }
 
-  public onDeactivate(event: Event): void {
-    console.debug(`[BrowserOutletComponent::sci-router-outlet:ondeactivate] [outlet=${this.outletName}, url=${(event as CustomEvent).detail}]`);
+  protected onDeactivate(event: Event): void {
+    console.debug(`[BrowserOutletComponent::sci-router-outlet:ondeactivate] [outlet=${this.outletName()}, url=${(event as CustomEvent).detail}]`);
   }
 
-  public onFocusWithin(event: Event): void {
-    console.debug(`[BrowserOutletComponent::sci-router-outlet:onfocuswithin] [outlet=${this.outletName}, focuswithin=${(event as CustomEvent).detail}]`);
+  protected onFocusWithin(event: Event): void {
+    console.debug(`[BrowserOutletComponent::sci-router-outlet:onfocuswithin] [outlet=${this.outletName()}, focuswithin=${(event as CustomEvent).detail}]`);
+  }
+
+  private navigate(): void {
+    void Beans.get(OutletRouter).navigate(this.form.controls.url.value, {outlet: this.outletName()});
   }
 
   private readAppEntryPoints(): AppEndpoint[] {
@@ -131,12 +119,18 @@ export class BrowserOutletComponent implements OnInit {
   /**
    * Listens for messages to update the context of this outlet.
    */
-  private installOutletContextUpdateHandler(outletName: string): void {
-    Beans.get(MessageClient).observe$(TestingAppTopics.routerOutletContextUpdateTopic(outletName, ':key'))
-      .pipe(takeUntilDestroyed(this._destroyRef))
-      .subscribe(message => {
-        this.routerOutlet.nativeElement.setContextValue(message.params!.get('key')!, message.body);
+  private installOutletContextUpdateHandler(): void {
+    effect(onCleanup => {
+      const outletName = this.outletName();
+      const routerOutlet = this._routerOutlet();
+
+      untracked(() => {
+        const subscription = Beans.get(MessageClient).observe$(TestingAppTopics.routerOutletContextUpdateTopic(outletName, ':key'))
+          .subscribe(message => routerOutlet.nativeElement.setContextValue(message.params!.get('key')!, message.body));
+
+        onCleanup(() => subscription.unsubscribe());
       });
+    });
   }
 }
 
